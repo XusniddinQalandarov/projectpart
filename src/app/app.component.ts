74731e28be701ddba4fe7344150f3024464be3dc
XusniddinQalandarov <x.qalandarov@newuzbekistanuniversity.uz>
2025-05-14 11:29:24 +0500

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ArgosComponent } from './argos/argos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ArgosComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'projectpart';
---
+++ src/app/argos/argos.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-argos',
+  imports: [],
+  templateUrl: './argos.component.html',
+})
+export class ArgosComponent {}
