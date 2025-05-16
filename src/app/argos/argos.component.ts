19fe4a480c146edcd53ecd7c7e4aeb990b5167f9
XusniddinQalandarov <x.qalandarov@newuzbekistanuniversity.uz>
2025-05-16 07:10:45 +0500

    total

--- src/app/argos/argos.component.ts
+++ src/app/argos/argos.component.ts
@@ -1,8 +1,26 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-argos',
-  imports: [],
+  imports: [NgIf, NgClass],
   templateUrl: './argos.component.html',
 })
-export class ArgosComponent {}
+export class ArgosComponent {
+  isDark = false;
+  currentLang = 'Uz';
+  languageMenuOpen = false;
+
+  toggleDarkMode() {
+    this.isDark = !this.isDark;
+  }
+
+  toggleLanguageMenu() {
+    this.languageMenuOpen = !this.languageMenuOpen;
+  }
+
+  selectLanguage(lang: string) {
+    this.currentLang = lang;
+    this.languageMenuOpen = false;
+  }
+}
