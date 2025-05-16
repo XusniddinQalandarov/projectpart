eeb178e0e8c052c4a0167cf8f82bd4566b91c054
XusniddinQalandarov <x.qalandarov@newuzbekistanuniversity.uz>
2025-05-16 10:14:52 +0500

    added transloco

--- src/app/argos/argos.component.ts
+++ src/app/argos/argos.component.ts
@@ -1,15 +1,23 @@
-import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslocoModule, TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-argos',
-  imports: [NgIf, NgClass],
+  standalone: true,
+  imports: [CommonModule, TranslocoModule],
   templateUrl: './argos.component.html',
 })
 export class ArgosComponent {
   isDark = false;
-  currentLang = 'Uz';
+  currentLang: 'uz' | 'en' | 'ru' = 'uz';
   languageMenuOpen = false;
+
+  readonly languages: Array<'uz' | 'en' | 'ru'> = ['uz', 'en', 'ru'];
+
+  constructor(private transloco: TranslocoService) {
+    this.transloco.setActiveLang(this.currentLang);
+  }
 
   toggleDarkMode() {
     this.isDark = !this.isDark;
@@ -19,8 +27,9 @@ export class ArgosComponent {
     this.languageMenuOpen = !this.languageMenuOpen;
   }
 
-  selectLanguage(lang: string) {
+  selectLanguage(lang: 'uz' | 'en' | 'ru') {
     this.currentLang = lang;
     this.languageMenuOpen = false;
+    this.transloco.setActiveLang(lang);
   }
 }
